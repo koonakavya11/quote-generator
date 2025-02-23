a94b5c42c69e8adecf4613d502dc019e4a7318bd
koonakavya11 <141955039+koonakavya11@users.noreply.github.com>
2025-02-23 21:37:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let btn = document.getElementById("quote_btn")
-// console.log(btn)
-btn = addEventListener("click",()=>{
-    let fetchdata = async()=>{
-        let data = await fetch("https://dummyjson.com/quotes/random")
+let btn = document.getElementById("quote_btn");
+
+btn.addEventListener("click", () => { 
+    // console.log(btn)
+    let fetchdata = async () => {
+        let data = await fetch("https://dummyjson.com/quotes/random");
         // console.log(data)
-        let quote_obj = await data.json()
+        let quote_obj = await data.json();
         // console.log(quote_obj)
-        let quote = quote_obj.quote
-        let auth = quote_obj.author
-        let text = document.getElementById("text")
-        let author= document.getElementById("author")
-        text.innerHTML =`<q>${quote}</q>`
-        author.innerHTML =`- ${auth}`
-}
-fetchdata()
-})
+        let quote = quote_obj.quote;
+        let auth = quote_obj.author;
+        let text = document.getElementById("text");
+        let author = document.getElementById("author");
+        
+        text.innerHTML = `<q>${quote}</q>`;
+        author.innerHTML = `- ${auth}`;
+    };
+    fetchdata();
+});
